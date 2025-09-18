d041fe45ff36c2c05177d33b1c506b38a66d7665
factmusingarimi <fmusingarimi@gmail.com>
2025-09-18 04:29:14 -0400

    add ch1/ex1.13

---
+++ ch1/ex1.13/sum.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int sum = 0;
+    for (int count = 50; count <= 100; ++count)
+    {
+        sum += count;
+    }
+    std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
+    return 0;
+}
